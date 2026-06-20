0f94178d1c9a2a5b402933da4b8d7460812a325d
kali <kali@kali>
2026-06-20 02:11:00 +0200

    Willkommens Fenster des mehr-erfahren Buttons ergänzt

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.getElementById("mehr-erfahren");
 
 button.addEventListener("click", function () {
 
-    alert("Willkommen auf meiner Homepage!");
+    alert("Willkommen auf meiner Homepage! Diese Seite befindet sich noch in der Entwicklungs- und Aufbauphase");
     document
     .getElementById("about")
     .scrollIntoView({
