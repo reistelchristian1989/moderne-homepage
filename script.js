c20ee32c7b6ecddb3370aa8c4d23d72c8a56c81a
reistelchristian1989 <reistelchristian1989@icloud.com>
2026-09-26 18:59:32 +0200

    Seite aktualisiert und neues Video erstellt und hochgeladen

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 console.log("JavaScript funktioniert");
 
-const button =
-document.getElementById("mehr-erfahren");
+const button = document.getElementById("mehr-erfahren");
 
-button.addEventListener("click", function () {
-
-    alert("Willkommen auf meiner Homepage! Diese Seite befindet sich noch in der Entwicklungs- und Aufbauphase");
-    document
-    .getElementById("about")
-    .scrollIntoView({
-        behavior: "smooth"
+if (button) {
+    button.addEventListener("click", function () {
+        alert("Willkommen auf meiner Homepage! Diese Seite befindet sich noch in der Entwicklungs- und Aufbauphase... NEU! cr.dev.monkey!!! Unter dem reiter cr.dev.monkey-Store wird ein kleiner eigener Store entstehen in dem es zu anfang Software, Apps, WebApplicationen geben wird. Natürlich OpenSource und frei zum Download. Hier werde ich aktuelle Projekte, Arbeiten von mir Präsentieren bevor sie in anderen Stores erhältlich ist.");
+        
+        const aboutSection = document.getElementById("about");
+        if (aboutSection) {
+            aboutSection.scrollIntoView({
+                behavior: "smooth"
+            });
+        }
     });
-
-});
+}
